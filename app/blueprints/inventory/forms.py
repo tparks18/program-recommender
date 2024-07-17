22b8ed53ab7f881a7e2885bbe72e4ce84c85f0c7
Taty <tatyanaparks@gmail.com>
2024-07-17 17:43:26 -0500

    changed routes

--- app/blueprints/inventory/forms.py
+++ app/blueprints/inventory/forms.py
@@ -14,10 +14,10 @@ class SearchForm(FlaskForm):
     submit = SubmitField('Search')
 
 class QuizForm(FlaskForm):
-    job_role = SelectField('Job Role', choices=[('principal', 'Principal'), ('assistant_principal', 'Assistant Principal'), ('teacher_leader', 'Teacher Leader: teachers, network staff, and school support staff')], validators=[DataRequired()])
-    years_experience = SelectField('Years of Experience', choices=[('0-1', '0-1'), ('2-5', '2-5'), ('6+', '6+')], validators=[DataRequired()])
+    job_role = SelectField('What is your current role?', choices=[('principal', 'Principal'), ('assistant_principal', 'Assistant Principal'), ('teacher_leader', 'Teacher Leader: teachers, network staff, and school support staff')], validators=[DataRequired()])
+    years_experience = SelectField('How long have you been in your current role', choices=[('0-1', '0-1'), ('2-5', '2-5'), ('6+', '6+')], validators=[DataRequired()])
     time_commitment = SelectMultipleField(
-    'Time Commitment',
+    'How much time can you commit to a program?',
     choices=[
         ('2_day_workshop', '2 Day Workshop'),
         ('4_5_90_minute_sessions', '4-5, 90 minute sessions throughout the first or second half of the school year'),
@@ -28,7 +28,7 @@ class QuizForm(FlaskForm):
     ],
     validators=[DataRequired()]
 )
-    topics_addressed = SelectMultipleField('Topics Addressed', choices=[('test_scores', 'Improving Test Scores'), ('innovation', 'Innovation'), ('leadership', 'Improving Leadership'), ('bilingual', 'Improving Bilingual Education')], validators=[DataRequired()])
+    topics_addressed = SelectMultipleField('What topics are you interested in?', choices=[('test_scores', 'Improving Test Scores'), ('innovation', 'Innovation'), ('leadership', 'Improving Leadership'), ('bilingual', 'Improving Bilingual Education')], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 class ProgramForm(FlaskForm):
--- app/blueprints/inventory/routes.py
+++ app/blueprints/inventory/routes.py
@@ -10,33 +10,6 @@ def index():
     title = 'Home'
     return render_template('index.html', title=title)
 
-@inventory.route('/all-items')
-def all_items():
-    title = 'All items'
-    items = Item.query.all()
-    return render_template('all_items.html', title=title, items=items)
-
-@inventory.route('/create-item', methods=['GET', 'POST'])
-@login_required
-def create_item():
-    title = 'Create a recommender template'
-    form = ItemForm()
-    if form.validate_on_submit():
-        title = form.title.data
-        description = form.description.data
-        cost = form.cost.data
-        new_item = Item(title=title, description=description, cost=cost, user_id=current_user.id)
-        flash(f"{new_item.title} has been created", 'is-success')
-        return redirect(url_for('inventory.index'))
-    return render_template('create_item.html', title=title, form=form)
-
-@inventory.route('/my-items')
-@login_required
-def my_items():
-    title = 'My Recommender Templates'
-    items = current_user.items.all()
-    return render_template('my_items.html', title=title, items=items)
-
 @inventory.route('/search-items', methods=['GET', 'POST'])
 def search_items():
     title = 'Search'
@@ -47,12 +20,6 @@ def search_items():
         items = Item.query.filter( (Item.title.ilike(f'%{term}%')) | (Item.description.ilike(f'%{term}%')) ).all()
     return render_template('search_items.html', title=title, items=items, form=form)
 
-@inventory.route('/item/<item_id>')
-@login_required
-def single_item(item_id):
-    item = Item.query.get_or_404(item_id)
-    title = item.title
-    return render_template('item_detail.html', title=title, item=item)
 
 @inventory.route('/edit-items/<item_id>', methods=["GET", "POST"])
 @login_required
@@ -80,14 +47,6 @@ def delete_item(item_id):
         item.delete()
         flash(f'{item.title} has been deleted.', 'is-success')
     return redirect(url_for('inventory.my_items'))
-
-@inventory.route('/table-of-items')
-@login_required
-def table_of_items():
-    context = {
-        'items': Item.query.all()
-    }
-    return render_template('table_of_items.html', **context)
 
 @inventory.route('/quiz', methods=['GET', 'POST'])
 def quiz():
