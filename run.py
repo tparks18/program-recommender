de857d2a0f56b05fcf2701ba6692a2fa700ba35e
Taty <tatyanaparks@gmail.com>
2024-07-18 09:46:25 -0500

    added debug logging

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ def make_context():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
+    logging.basicConfig(level=logging.DEBUG)
     app.run(host='0.0.0.0', port=port)
