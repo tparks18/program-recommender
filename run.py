cf4aeddfdcb12086a7034d6b63018bc4a0c2ab89
Taty <tatyanaparks@gmail.com>
2024-07-18 10:51:29 -0500

    changed run.py back and init back

--- run.py
+++ run.py
@@ -10,3 +10,19 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+# from app import create_app, db
+# from app.blueprints.auth.models import User
+# from app.blueprints.inventory.models import Program
+# from flask_migrate import Migrate
+
+# app = create_app()
+# migrate = Migrate(app, db)
+
+# @app.shell_context_processor
+# def make_context():
+#     return {'db': db, 'User': User, 'Program': Program}
+
+# if __name__ == "__main__":
+#     import os
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
