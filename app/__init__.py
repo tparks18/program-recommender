a6388e10180083c94cdd25c99e4c7bb987aa1646
Taty <tatyanaparks@gmail.com>
2024-07-18 11:25:35 -0500

    removed commented out code

--- app/__init__.py
+++ app/__init__.py
@@ -21,34 +21,3 @@ from app.blueprints.inventory import inventory
 app.register_blueprint(inventory)
 
 from app import routes
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from flask_login import LoginManager
-# from config import Config
-
-# db = SQLAlchemy()
-# migrate = Migrate()
-# login = LoginManager()
-
-# def create_app(config_class=Config):
-#     app = Flask(__name__)
-#     app.config.from_object(config_class)
-
-#     db.init_app(app)
-#     migrate.init_app(app, db)
-#     login.init_app(app)
-#     login.login_view = 'auth.login'
-#     login.login_message_category = 'is-danger'
-
-#     from app.blueprints.auth import auth
-#     app.register_blueprint(auth)
-
-#     from app.blueprints.inventory import inventory
-#     app.register_blueprint(inventory)
-
-#     from app import routes
-
-#     return app
-
--- config.py
+++ config.py
@@ -1,20 +1,3 @@
-# import os
-
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-# class Config:
-#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-#     DATABASE_URL = os.environ.get('DATABASE_URL')
-    
-#     if DATABASE_URL and 'postgres://' in DATABASE_URL:
-#         SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace('postgres://', 'postgresql://')
-#     elif DATABASE_URL:
-#         SQLALCHEMY_DATABASE_URI = DATABASE_URL
-#     else:
-#         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 import os
 from dotenv import load_dotenv
 
