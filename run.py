f8e32486afdfd25542790944a9737b60c7603cdf
Taty <tatyanaparks@gmail.com>
2024-07-17 20:30:55 -0500

    added deployment files

--- run.py
+++ run.py
@@ -2,7 +2,11 @@ from app import app, db
 from app.blueprints.auth.models import User
 from app.blueprints.inventory.models import Program
 
-
 @app.shell_context_processor
 def make_context():
     return {'db': db, 'User': User, 'Program': Program}
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
