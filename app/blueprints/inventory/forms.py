94723fb6d1dd7a583f79c32bddc7b505e0caf4ea
Taty <tatyanaparks@gmail.com>
2024-07-25 11:30:46 -0500

    reverted forms

--- app/blueprints/inventory/forms.py
+++ app/blueprints/inventory/forms.py
@@ -4,12 +4,12 @@ from wtforms.validators import DataRequired
 
 class QuizForm(FlaskForm):
     job_role = SelectField('What is your current role?', choices=[('principal', 'Principal'), ('assistant_principal', 'Assistant Principal'), ('teacher_leader', 'Teacher Leader: teachers, network staff, and school support staff')], validators=[DataRequired()])
-    years_experience = SelectField('How long have you been in your current role?', choices=[('0-1', '0-1'), ('2-5', '2-5'), ('6+', '6+')], validators=[DataRequired()])
+    years_experience = SelectField('How long have you been in your current role', choices=[('0-1', '0-1'), ('2-5', '2-5'), ('6+', '6+')], validators=[DataRequired()])
     time_commitment = SelectMultipleField(
     'How much time can you commit to a program?',
     choices=[
         ('2_day_workshop', '2 Day Workshop'),
-        ('4_to_5_90_minute_sessions', '4 to 5, 90 minute sessions throughout the first or second half of the school year'),
+        ('4_5_90_minute_sessions', '4-5, 90 minute sessions throughout the first or second half of the school year'),
         ('monthly_90_minute_sessions', 'Monthly, 90 minute sessions throughout the school year'),
         ('monthly_120_minute_sessions', 'Monthly, 120 minute sessions throughout the school year'),
         ('1_four_hour_session', '1 four-hour session'),
@@ -58,7 +58,7 @@ class ProgramForm(FlaskForm):
     ], validators=[DataRequired()])
     time_commitment = SelectField('Time Commitment', choices=[
         ('2_day_workshop', '2 Day Workshop'),
-        ('4_to_5_90_minute_sessions', '4 to 5, 90 minute sessions throughout the first or second half of the school year'),
+        ('4_5_90_minute_sessions', '4-5, 90 minute sessions throughout the first or second half of the school year'),
         ('monthly_90_minute_sessions', 'Monthly, 90 minute sessions throughout the school year'),
         ('monthly_120_minute_sessions', 'Monthly, 120 minute sessions throughout the school year'),
         ('1_four_hour_session', '1 four-hour session'),
